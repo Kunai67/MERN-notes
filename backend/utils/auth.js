eb2837446f1f114d2ebf3da218eb2334c2d24b3a
Ian Agustin <inbagustin@gmail.com>
2020-05-02 23:29:18 +0800

    Refactored auth middleware

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,15 +1,14 @@
 // Authentication Middleware
-const express = require('express');
 const jwt = require('jsonwebtoken');
 
-function auth(req, res, next) {
+function auth (req, res, next) {
     // Check if request has auth-token
     const token = req.header('auth-token');
 
     if (!token) return res.status(401).json({ message: 'Access Denied' });
 
-    // Check if token is valid
     try {
+        // Check if token is valid
         const data = jwt.verify(token, process.env.SECRET_KEY);
         if (!data) return res.status(401).json({ message: 'Invalid or tampered token' });
 
@@ -18,8 +17,6 @@ function auth(req, res, next) {
     } catch (error) {
         res.status(401).json({ message: error.message });
     }
-
-    
 }
 
 module.exports = auth;
