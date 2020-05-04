610c5877222711205f2db18ea63dcf4197d3e621
Ian Agustin <inbagustin@gmail.com>
2020-05-04 19:50:12 +0800

    Connected redux to react

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,33 +6,35 @@ import UpdatePage from './pages/Update.page';
 import HomePage from './pages/Home.page';
 import RegisterPage from './pages/Register.page';
 import NotesNav from './components/Nav.component';
-
-require('./redux/store');
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 function App() {
   const [ isAuthenticated, Authenticate ] = useState(false);
 
   return (
-    <Router>
-      <NotesNav isAuthenticated={isAuthenticated} toggleAuth={Authenticate} />
-      <Switch>
-      {
-        !isAuthenticated ? 
-        <>
-          <Route exact path="/">
-            <HomePage toggleAuth={Authenticate} />
-          </Route>
-          <Route path="/register" component={RegisterPage} />
-        </>
-          :
-        <>
-          <Route path="/" component={NotesPage} />
-          <Route path="/new" component={FormPage} />
-          <Route path="/update" component={UpdatePage} />
-        </>
-      }
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <NotesNav isAuthenticated={isAuthenticated} toggleAuth={Authenticate} />
+        <Switch>
+        {
+          !isAuthenticated ? 
+          <>
+            <Route exact path="/">
+              <HomePage toggleAuth={Authenticate} />
+            </Route>
+            <Route path="/register" component={RegisterPage} />
+          </>
+            :
+          <>
+            <Route path="/" component={NotesPage} />
+            <Route path="/new" component={FormPage} />
+            <Route path="/update" component={UpdatePage} />
+          </>
+        }
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
