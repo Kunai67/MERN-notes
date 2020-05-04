6b8a38b4d2d4b53d443283e380dbbaef23c0af93
Ian Agustin <inbagustin@gmail.com>
2020-05-04 21:01:51 +0800

    Added a 404 page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import UpdatePage from './pages/Update.page';
 import HomePage from './pages/Home.page';
 import RegisterPage from './pages/Register.page';
 import NotesNav from './components/Nav.component';
+import ErrorPage from './pages/404.page';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
@@ -24,12 +25,14 @@ function App() {
               <HomePage toggleAuth={Authenticate} />
             </Route>
             <Route path="/register" component={RegisterPage} />
+            <Route component={ErrorPage} />
           </>
             :
           <>
             <Route exact path="/" component={NotesPage} />
             <Route path="/new" component={FormPage} />
             <Route path="/update" component={UpdatePage} />
+            <Route component={ErrorPage} />
           </>
         }
         </Switch>
---
+++ front-end/src/pages/404.page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+export default function ErrorPage() {
+    return (
+        <Container className="d-flex py-5 justify-content-center">
+            <h1 className='text-danger'>Uh oh! Page not found!</h1>
+        </Container>
+    )
+}
